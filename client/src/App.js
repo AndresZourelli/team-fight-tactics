3ff8d798b1ce0c7f9418c098c697dadd31e92cb5
Andres <azourelli@gmail.com>
2019-07-16 17:27:09 -0500

    added characters, origins, and classes to redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import DropContainer from './components/DropContainer';
 import DragContainer from './components/DragContainer';
 import CharacterJSON from './HOC/CharacterJSON';
 import CharacterOrigins from './components/Character_Origins/CharacterOrigins';
-function App(props) {
-	console.log(props);
-	return (
-		<div className="App">
-			<nav className="navbar navbar-dark bg-dark">
-				<a className="navbar-brand" href="/">
-					Team-Fight-Tactics
-				</a>
-			</nav>
-			<div className="main-container">
-				<div className="left-menu">Characters</div>
-				<div className="main-menu">Team Build Area</div>
-				<div className="right-menu">Item Area</div>
-				<div className="bottom-menu">
-					<CharacterOrigins />
+import { getCharacters } from './ReduxContainer/actions/actions';
+import { connect } from 'react-redux';
+class App extends Component {
+	componentDidMount() {
+		this.props.getCharacters();
+	}
+	render() {
+		return (
+			<div className="App">
+				<nav className="navbar navbar-dark bg-dark">
+					<a className="navbar-brand" href="/">
+						Team-Fight-Tactics
+					</a>
+				</nav>
+				<div className="main-container">
+					<div className="left-menu">Characters</div>
+					<div className="main-menu">Team Build Area</div>
+					<div className="right-menu">Item Area</div>
+					<div className="bottom-menu">
+						<CharacterOrigins />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
-export default CharacterJSON(App);
+// const mapStateToProps = (state) => ({
+
+// })
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getCharacters: () => {
+			dispatch(getCharacters());
+		}
+	};
+};
+
+export default connect(null, mapDispatchToProps)(App);
 
 // working code
 // <div className="App" style={{ display: 'flex' }}>
--- client/src/ReduxContainer/actions/types.js
+++ client/src/ReduxContainer/actions/types.js
@@ -1,1 +1,3 @@
 export const CHARACTERS = 'CHARACTERS';
+export const CLASSES = 'CLASSES';
+export const ORIGINS = 'ORIGINS';
