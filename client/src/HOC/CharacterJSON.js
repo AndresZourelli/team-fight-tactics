3ad2d9e7b5a070dcc26582afc4bceba6d70514ca
Andres <azourelli@gmail.com>
2019-07-15 21:30:38 -0500

    fixed json

--- client/src/HOC/CharacterJSON.js
+++ client/src/HOC/CharacterJSON.js
@@ -24,78 +24,64 @@ export default CharacterJSON;
 
 function extract_groups() {
 	console.log('Hi');
-	let demon = [];
-	let dragon = [];
-	let exile = [];
-	let glacial = [];
-	let robot = [];
-	let imperial = [];
-	let noble = [];
-	let ninja = [];
-	let pirate = [];
-	let phantom = [];
-	let wild = [];
-	let voids = [];
-	let yordle = [];
-
+	let assassin = [];
+	let blademaster = [];
+	let brawler = [];
+	let elementalist = [];
+	let guardian = [];
+	let gunslinger = [];
+	let knight = [];
+	let ranger = [];
+	let shapeshifter = [];
+	let sorcerer = [];
+	console.log(ListChar.characters.length);
 	for (let i = 0; i < ListChar.characters.length; i++) {
 		let index = ListChar.characters[i];
 
-		if (index.origins.includes('Demon')) {
-			demon.push(index.name);
+		if (index.class.includes('Assassin')) {
+			console.log('object');
+			assassin.push(index.name);
 		}
-		if (index.origins.includes('Dragon')) {
-			dragon.push(index.name);
+		if (index.class.includes('Blademaster')) {
+			blademaster.push(index.name);
 		}
-		if (index.origins.includes('Exile')) {
-			exile.push(index.name);
+		if (index.class.includes('Brawler')) {
+			brawler.push(index.name);
 		}
-		if (index.origins.includes('Glacial')) {
-			glacial.push(index.name);
+		if (index.class.includes('Elementalist')) {
+			elementalist.push(index.name);
 		}
-		if (index.origins.includes('Robot')) {
-			robot.push(index.name);
+		if (index.class.includes('Guardian')) {
+			guardian.push(index.name);
 		}
-		if (index.origins.includes('Imperial')) {
-			imperial.push(index.name);
+		if (index.class.includes('Gunslinger')) {
+			gunslinger.push(index.name);
 		}
-		if (index.origins.includes('Noble')) {
-			noble.push(index.name);
+		if (index.class.includes('Knight')) {
+			knight.push(index.name);
 		}
-		if (index.origins.includes('Ninja')) {
-			ninja.push(index.name);
+		if (index.class.includes('Ranger')) {
+			ranger.push(index.name);
 		}
-		if (index.origins.includes('Pirate')) {
-			pirate.push(index.name);
+		if (index.class.includes('Shapeshifter')) {
+			shapeshifter.push(index.name);
 		}
-		if (index.origins.includes('Phantom')) {
-			phantom.push(index.name);
-		}
-		if (index.origins.includes('Wild')) {
-			wild.push(index.name);
-		}
-		if (index.origins.includes('Void')) {
-			voids.push(index.name);
-		}
-		if (index.origins.includes('Yordle')) {
-			yordle.push(index.name);
+		if (index.class.includes('Sorcerer')) {
+			sorcerer.push(index.name);
 		}
 	}
 
 	let finished_char_obj = {
-		demon: demon,
-		dragon: dragon,
-		exile: exile,
-		glacial: glacial,
-		robot: robot,
-		imperial: imperial,
-		noble: noble,
-		ninja: ninja,
-		pirate: pirate,
-		phantom: phantom,
-		wild: wild,
-		voids: voids,
-		yordle: yordle
+		assassin: assassin,
+		blademaster: blademaster,
+		brawler: brawler,
+		elementalist: elementalist,
+		guardian: guardian,
+		gunslinger: gunslinger,
+		knight: knight,
+		ranger: ranger,
+		shapeshifter: shapeshifter,
+		sorcerer: sorcerer
 	};
 
 	return finished_char_obj;
