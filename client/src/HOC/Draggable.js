6c6f3b69868073e299d26ff9877b3c48ac9838c2
Andres <azourelli@gmail.com>
2019-07-17 21:42:09 -0500

    added console.logs

--- client/src/HOC/Draggable.js
+++ client/src/HOC/Draggable.js
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types';
 const Draggable = (WrappedComponent) => {
 	return class extends Component {
 		drag = (e) => {
+			console.log('Drag', e);
 			e.dataTransfer.setData('transfer', e.target.id);
+			e.dataTransfer.effectAllowed = 'copy';
 		};
 
 		noAllowDrop = (e) => {
 			e.stopPropagation();
 		};
 		render() {
-			console.log(this.props);
 			return (
-				<WrappedComponent draggable onDragStart={this.drag} onDragOver={this.noAllowDrop} {...this.props}>
+				<WrappedComponent
+					draggable
+					onDragStart={this.drag}
+					onDragOver={this.noAllowDrop}
+					{...this.props}
+					hi="hi">
 					{this.props.children}
 				</WrappedComponent>
 			);
